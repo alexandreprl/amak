bfc08ba8092a16b2bb55f66beaf388ed1c315966
Alexandre <aknot31@gmail.com>
2018-06-01 10:56:04 +0200

    Added ability to change main window title and icon

--- src/fr/irit/smac/amak/Information.java
+++ src/fr/irit/smac/amak/Information.java
@@ -11,7 +11,7 @@ public class Information {
 	/**
 	 * The version of AMAK
 	 */
-	public static final String VERSION = "1.4.3";
+	public static final String VERSION = "1.4.3.1";
 
 	/**
 	 * Private constructor to avoid instantiation
--- src/fr/irit/smac/amak/ui/MainWindow.java
+++ src/fr/irit/smac/amak/ui/MainWindow.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.util.concurrent.locks.ReentrantLock;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -44,66 +45,89 @@ public class MainWindow extends JFrame {
 		toolbarPanel = new JPanel();
 		getContentPane().add(toolbarPanel, BorderLayout.SOUTH);
 		toolbarPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-		
+
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 		getContentPane().add(splitPane, BorderLayout.CENTER);
-		
+
 		tabbedPanel = new JTabbedPane();
 		splitPane.setRightComponent(tabbedPanel);
-		
+
 		menuBar = new JMenuBar();
 		optionsMenu = new JMenu("Options");
 		menuBar.add(optionsMenu);
 		getContentPane().add(menuBar, BorderLayout.NORTH);
-		
+
 		JMenuItem menuItem = new JMenuItem("Close");
-		menuItem.addActionListener(l->{
+		menuItem.addActionListener(l -> {
 			System.exit(0);
 		});
 		optionsMenu.add(menuItem);
 
-		menuBar.add(new JMenu("AMAK v"+Information.VERSION));
-		
+		menuBar.add(new JMenu("AMAK v" + Information.VERSION));
+
 		setVisible(true);
-			
+
 	}
+
+	/**
+	 * Change the icon of the window
+	 * 
+	 * @param filename
+	 *            The filename of the icon
+	 */
+	public static void setWindowIcon(String filename) {
+		ImageIcon img = new ImageIcon(filename);
+		instance().setIconImage(img.getImage());
+	}
+
+	/**
+	 * Change the title of the main window
+	 * 
+	 * @param title The new title
+	 */
+	public static void setWindowTitle(String title) {
+		instance().setTitle(title);
+	}
+
 	public static void addMenuItem(String title, ActionListener listener) {
 		JMenuItem menuItem = new JMenuItem(title);
 		menuItem.addActionListener(listener);
 		instance().optionsMenu.add(menuItem);
 	}
+
 	/**
 	 * Add a toolBar
 	 * 
 	 * @param toolbar
-	 * 			The ToolBar.
+	 *            The ToolBar.
 	 */
 	public static void addToolbar(JToolBar toolbar) {
 		instance().toolbarPanel.add(toolbar);
 		instance().pack();
 		instance().setVisible(true);
 	}
-	
+
 	/**
 	 * Add a tabbedPanel
 	 * 
 	 * @param title
-	 * 			The title
+	 *            The title
 	 * @param panel
-	 * 			The panel
+	 *            The panel
 	 */
-	
+
 	public static void setLeftPanel(String title, JPanel panel) {
 		instance().splitPane.setLeftComponent(panel);
 		instance().pack();
 		instance().setVisible(true);
 	}
+
 	public static void setRightPanel(String title, JPanel panel) {
 		instance().splitPane.setRightComponent(panel);
 		instance().pack();
 		instance().setVisible(true);
 	}
-	
+
 	/**
 	 * Return the unique instance of MainWindow, may create it.
 	 * 
@@ -117,6 +141,7 @@ public class MainWindow extends JFrame {
 		instanceLock.unlock();
 		return instance;
 	}
+
 	public static void addTabbedPanel(String title, JPanel panel) {
 		instance().tabbedPanel.addTab(title, panel);
 		instance().pack();
