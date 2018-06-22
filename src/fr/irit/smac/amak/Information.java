a9ef5dcdd5ded501d651a050cb68adc971f244d5
Alexandre <aknot31@gmail.com>
2018-06-22 15:59:16 +0200

    JAR version with commandLineOnly option

--- src/fr/irit/smac/amak/Information.java
+++ src/fr/irit/smac/amak/Information.java
@@ -11,7 +11,7 @@ public class Information {
 	/**
 	 * The version of AMAK
 	 */
-	public static final String VERSION = "1.4.3.2";
+	public static final String VERSION = "1.4.3.3";
 
 	/**
 	 * Private constructor to avoid instantiation
