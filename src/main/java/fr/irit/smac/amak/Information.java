2a11e0d92fb4a2e17cef5a8cc460eeebc961d123
Alexandre <alexandre.perles@irit.fr>
2021-06-17 09:32:24 +0200

    Bump to version 2.0.0

--- src/main/java/fr/irit/smac/amak/Information.java
+++ src/main/java/fr/irit/smac/amak/Information.java
@@ -11,7 +11,7 @@ public class Information {
 	/**
 	 * The version of AMAK
 	 */
-	public static final String VERSION = "1.5.3";
+	public static final String VERSION = "2.0.0";
 
 	/**
 	 * Private constructor to avoid instantiation
